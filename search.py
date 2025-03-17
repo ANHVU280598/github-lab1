90b6fcf486af0cfae6e3d0d9be2707a810ca330e
anhvulenovo <anhvu280598@gmail.com>
2025-03-17 07:40:58 -0500

    search function implement ENPM611

--- search.py
+++ search.py
@@ -1,8 +1,45 @@
 import json
+import statistics
+
+def load_data(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("JSON file not found.")
+        return []
+    except json.JSONDecodeError:
+        print("Invalid JSON format.")
+        return []
 
 def search_json(json_data, search_string):
     results = []
-    # Place your search code here
-    # you will have to loop through the json_data file you created earlier
-    # finally you can store the match in the result list and return it
+    keyword = search_string.lower()
+
+    related_items = {}
+
+    for entry in json_data:
+        for key, value in entry.items():
+            if keyword in key.lower() and key.lower() != "user":
+                try:
+                    num_value = float(value)
+                    if key not in related_items:
+                        related_items[key] = []
+                    related_items[key].append(num_value)
+                except ValueError:
+                    continue 
+
+    if not related_items:
+        return {"No Item Found"}
+
+    for item, reviews in related_items.items():
+        results.append({
+            "Item": item,
+            "Total Reviews": len(reviews),
+            "Average Review": round(statistics.mean(reviews), 2),
+            "Minimum Review": min(reviews),
+            "Maximum Review": max(reviews),
+        })
+
+    
     return results
